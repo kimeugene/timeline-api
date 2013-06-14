5088cc639f78cd1a52e6026b3574782712822525
Eugene Kim <eugk5@gmail.com>
2013-06-14 03:01:57 +0000

    Added a basic algorithm to determine how to cut out extra data points depending on zoom level. Needs work, but... works.

--- httpdocs/map.php
+++ httpdocs/map.php
@@ -9,121 +9,123 @@
       #map_canvas { height: 100% }
     </style>
     <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB3X-jzV35dR45FDEq2GC9lgF19RfI3Jm4&sensor=false"></script>
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <script type="text/javascript">
 
-      $(document).ready(function() {
-        // Map initialization
-        var map;
-        var overlays = [];
-        var markers = [];
-        var infowindows = [];
-        function initialize() {
-          var mapOptions = {
-            center: new google.maps.LatLng(40.763028, -74.024498),
-            zoom: 13,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-          };
-          map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-        }
+$(document).ready(function() {
+    // Map initialization
+    var map;
+    var overlays = [];
+    var markers = [];
+    var infowindows = [];
 
-
-function listenMarker (marker, infowindow)
-{
-    // so marker is associated with the closure created for the listenMarker function call
-
-      google.maps.event.addListener(marker, 'click', function() {
-        infowindow.open(map,marker);
+    function initialize() {
+        var current = new google.maps.LatLng(40.763028, -74.024498);
+        var mapOptions = {
+          center: current,
+          zoom: 13,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+        $('#zoom').val(mapOptions.zoom);
+        
+        google.maps.event.addListener(map, 'zoom_changed', function() {
+            update();
       });
-}
+    }
 
-        // Mobile Device support
-        function detectBrowser() {
-          var useragent = navigator.userAgent;
-          var mapdiv = document.getElementById("map_canvas");
+    function listenMarker (marker, infowindow){
+        // so marker is associated with the closure created for the listenMarker function call
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.open(map,marker);
+        });
+    }
 
-          if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1 ) {
+    // Mobile Device support
+    function detectBrowser() {
+        var useragent = navigator.userAgent;
+        var mapdiv = document.getElementById("map_canvas");
+
+        if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1 ) {
             mapdiv.style.width = '100%';
             mapdiv.style.height = '100%';
-          } 
-        }
-        initialize();
-	detectBrowser();
+        } 
+    }
 
-        // Click Handlers
-        $('#fetch').click(function() {
-	  email = $('#email').val();
-          date = $('#date').val();
-          datapoints = $('#datapoints').val();
-	  if(email.length == 0) {
-	    alert('Please enter the email address key for DynamoDB and try again.');
-	    return;
-	  } 
-	   
-	  $.getJSON('/get/'+email+'/'+date+'/'+datapoints, function(response) {
+    function update(){
+        email = $('#email').val();
+        date = $('#date').val();
+        datapoints = $('#datapoints').val();
+        zoom = map.getZoom();
+
+        if(email.length == 0) {
+            alert('Please enter the email address key for DynamoDB and try again.');
+            return;
+        }
+
+//        console.log('Zoom level: ' + zoom);
+        $.getJSON('/get/'+email+'/'+date+'/'+ zoom+'/'+datapoints, function(response) {
             for(i=0; i<overlays.length; i++) {
               overlays[i].setMap(null);
             }
 
-if (markers) {
-    for (i in markers) {
-      markers[i].setMap(null);
-    }
-  }
+            if (markers) {
+                for (i in markers) {
+                  markers[i].setMap(null);
+                }
+            }
 
             overlays = [];
-	    markers = [];
+            markers = [];
+
             var flightPlanCoordinates = [];
             for(i=0; i<response.length; i++) {
-              flightPlanCoordinates.push(new google.maps.LatLng(response[i][0], response[i][1]));
+                flightPlanCoordinates.push(new google.maps.LatLng(response[i][0], response[i][1]));
+                var myLatlng = new google.maps.LatLng(response[i][0], response[i][1]);
+                var mapOptions = {
+                    zoom: 4,
+                    center: myLatlng,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                }
+                var contentString = response[i][2];
+                var infowindow = new google.maps.InfoWindow({
+                    content: contentString
+                });
+                infowindows.push(infowindow);
+                var marker = new google.maps.Marker({
+                    position: myLatlng,
+                    map: map,
+                    title: ""
+                });
 
-  var myLatlng = new google.maps.LatLng(response[i][0], response[i][1]);
-  var mapOptions = {
-    zoom: 4,
-    center: myLatlng,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  }
-
-var contentString = response[i][2];
-var infowindow = new google.maps.InfoWindow({
-    content: contentString
-});
-infowindows.push(infowindow);
-
-  var marker = new google.maps.Marker({
-      position: myLatlng,
-      map: map,
-      title: ""
-  });
-
-listenMarker(marker, infowindow);
-  markers.push(marker);
-
+                listenMarker(marker, infowindow);
+                markers.push(marker);
             }
 
             var flightPath = new google.maps.Polyline({
-              path: flightPlanCoordinates,
-              strokeColor: "#000",
-              strokeOpacity: 0.5,
-              strokeWeight: 4
+                path: flightPlanCoordinates,
+                strokeColor: "#000",
+                strokeOpacity: 0.5,
+                strokeWeight: 4
             });
-            flightPath.setMap(map);	    
-	    overlays.push(flightPath);
+            flightPath.setMap(map);        
+            overlays.push(flightPath);
+        });        
+        return false;
+    }
 
+    initialize();
+    detectBrowser();
 
+    // Click Handlers
+    $('#fetch').click(update());
+    $('#togglePanel').click(function() {
+        $('#overlay').toggle();
+    });
 
-	  });		
-          return false;
- 	});
-
-        $('#togglePanel').click(function() {
-          $('#overlay').toggle();
-        });
-
-        // Click this in the beginning
-        $('#fetch').click();
-
-      });
+    // Click this in the beginning
+    $('#fetch').click();
+});
     </script>
   </head>
   <body>
@@ -133,7 +135,7 @@ listenMarker(marker, infowindow);
     <form>
     <div id="overlay" style="z-index:5; width: 200px;position: absolute; right: 5px; top: 40px; background-color: #fff; border: 1px solid #000; opacity: .75; padding: 10px; box-shadow: 3px 3px 5px #777; border-radius: 3px;" >
      <b>Timeline User ID:</b><br>
-     <input type="text" id="email" style="width: 150px; height: 16px; margin-top: 5px; margin-bottom: 8px; padding: 5px;" value="fitz5@timeline.pwn" /><br>
+     <input type="text" id="email" style="width: 150px; height: 16px; margin-top: 5px; margin-bottom: 8px; padding: 5px;" value="pwnage" /><br>
 
      <div style="width: 100%; margin-top: 5px; margin-bottom: 8px;">
        <div style="width: 50%; float: left;">
@@ -146,6 +148,7 @@ listenMarker(marker, infowindow);
        </div>
      </div>
 
+     <input type="hidden" id="zoom" value="" />
      <input type="submit" id="fetch" value="Fetch" style="width: 100%; margin-top: 3px;" />
     </div>
     </form>
--- include/controller/GetData.php
+++ include/controller/GetData.php
@@ -3,9 +3,6 @@ namespace Timeline;
 
 class GetData extends Common
 {
-    // Minimum distance required to be considered a new point.
-    const MINIMUM_DISTANCE = 300; // In meters
-    
     // Units of distance
     const MILES = 'M';
     const KILOMETERS = 'K';
@@ -24,6 +21,7 @@ class GetData extends Common
 
     public function process($params)
     {
+        $this->zoomLevel = $params['zoom'];
         $data = array(
             'TableName'         => 'history_new',
             'HashKeyValue'      => array(\Aws\DynamoDb\Enum\ScalarAttributeType::S => $params['user_id']),
@@ -102,7 +100,7 @@ class GetData extends Common
         $distance = self::vincentyGreatCircleDistance($this->pointOfOrigin, $currentPoint);
         $this->log->addDebug('Distance calculated: ' . $distance);
         
-        if ($distance <= self::MINIMUM_DISTANCE) {
+        if ($distance <= $this->calculateDistance($this->zoomLevel)) {
             $this->log->addDebug(sprintf('Point discarded; distance was too low: %d.', $distance));
             return false;
         }
@@ -125,6 +123,46 @@ class GetData extends Common
 
         $angle = atan2(sqrt($a), $b);
         return $angle * $earthRadius;
+    }
+    
+    /**
+     * Distance needed to be considered a new point
+     * @todo We need to find a better algorithm for this. The number right now is arbitrary and, frankly, not immensely convincing.
+     * @param type $zoom
+     * @return type
+     */
+    private function calculateDistance($zoom)
+    {
+        $scale = array(
+            0 => 591657550.500000,
+            1 => 591657550.500000,
+            2 => 295828775.300000,
+            3 => 147914387.600000,
+            4 => 73957193.820000,
+            5 => 36978596.910000,
+            6 => 18489298.450000,
+            7 => 9244649.227000,
+            8 => 4622324.614000,
+            9 => 2311162.307000,
+            10 => 1155581.153000,
+            11 => 577790.576700,
+            12 => 288895.288400,
+            13 => 144447.644200,
+            14 => 72223.822090,
+            15 => 36111.911040,
+            16 => 18055.955520,
+            17 => 9027.977761,
+            18 => 4513.988880,
+            19 => 2256.994440,
+            20 => 1128.497220,
+            21 => 564.24861
+        );
+        
+        $distance = $scale[$zoom] / pow(1.58, $zoom);
+        $distance = ($distance < 300) ? 300 : $distance;
+        $this->log->addDebug(sprintf('Zoom: %d, Distance calculated: %d.', $zoom, $distance));
+        
+        return $distance;
     }
     
 }
